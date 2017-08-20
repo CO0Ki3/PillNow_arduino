c491dd2293ee2deba7e060bcae2d35b1f3b08e0c
co0ki3 <ehrrhgus0324@gmail.com>
2017-08-20 18:38:42 +0900

    cds value is not complete

---
+++ PillNow_arduino.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+int cds = A0;
+SoftwareSerial BTSerial(2,3); // TX : 2 || RX : 3
+
+void setup()
+{
+  BTSerial.begin(9600);
+  Serial.begin(9600);
+  BTSerial.println("C");
+}
+
+void loop()
+{
+  int cdsValue = analogRead(cds);
+  Serial.print("cds = ");
+  Serial.println(cdsValue);
+  if(cdsValue <= 300) {
+    BTSerial.println("O");
+  }
+}
